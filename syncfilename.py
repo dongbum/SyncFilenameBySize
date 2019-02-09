aabb2d9437099429544629fad0fd44d5c50c8018
DONGBUM, KIM <dongbum9@gmail.com>
2019-02-09 15:26:05 +0900

    Basic test complete.

--- syncfilename.py
+++ syncfilename.py
@@ -1,22 +1,26 @@
 import sys
 import glob
+import os
 from os.path import *
 
 if len(sys.argv) < 2:
-    print("Usage : BASE TARGET")
+    print("Usage : python syncfilename.py BASE_DIR TARGET_DIR")
     exit(0)
 
-print("옵션1 : " + sys.argv[0])
-print("옵션2 : " + sys.argv[1])
-print("옵션3 : " + sys.argv[2])
-
-for filename in glob.glob(abspath(sys.argv[1]) + '\\*'):
-    print(filename + " - " + str(getsize(filename)))
-
+target_dir = sys.argv[2]
 target_dict = dict()
 
+# 타겟의 파일들을 크기와 파일명으로 구분하여 입력
 for filename in glob.glob(abspath(sys.argv[2] + '\\*')):
-    print(filename + " - " + str(getsize(filename)))
-    target_dict.get(getsize(filename))
-    target_dict.update(getsize(filename), filename)
+    # print(filename + " - " + str(getsize(filename)))
+    if (None != target_dict.get(getsize(filename))):
+        print("Same file size detected.")
+        exit(0)
+
+    target_dict[getsize(filename)] = filename
+
+# 대상의 파일들을 검색
+for filename in glob.glob(abspath(sys.argv[1]) + '\\*'):
+    os.rename(target_dict.get(getsize(filename)), target_dir + '\\' + basename(filename))
+
 
